bfdd41f39758df1c046db9552d462e13b9842cd5
Qurbaaan <qurbaan.a1y@gmail.com>
2024-08-07 17:09:03 +0500

    adding chages

--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,13 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    '@fullhuman/postcss-purgecss': {
+      content: [
+          './pages/**/*.{js,jsx,ts,tsx}',
+          './components/**/*.{js,jsx,ts,tsx}'
+      ],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+    },
+    'postcss-preset-env': {},
   },
 };
