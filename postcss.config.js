ed81f07de23cf3e3ad335f5b01b6eb3ae7250ba3
Qurbaaan <qurbaan.a1y@gmail.com>
2024-08-07 17:14:58 +0500

    adding changes

--- postcss.config.js
+++ postcss.config.js
@@ -3,13 +3,5 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    '@fullhuman/postcss-purgecss': {
-      content: [
-          './pages/**/*.{js,jsx,ts,tsx}',
-          './components/**/*.{js,jsx,ts,tsx}'
-      ],
-      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-    },
-    'postcss-preset-env': {},
   },
 };
